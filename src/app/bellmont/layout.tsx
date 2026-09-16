ecd4844bdd53c1818015398acb630fb84e1a0691
Exsprezzollc <exsprezzollc@gmail.com>
2026-09-16 06:16:16 -0500

    Load Bellmont dropdown navigation styles

--- src/app/bellmont/layout.tsx
+++ src/app/bellmont/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "./bellmont.css";
+import "./bellmont-nav.css";
 
 export default function BellmontLayout({ children }: { children: ReactNode }) {
   return (
