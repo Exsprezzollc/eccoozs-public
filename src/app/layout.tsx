6d76bed396e835b4b6110e01569e3d4c3d96a29e
Exsprezzollc <exsprezzollc@gmail.com>
2026-09-19 20:47:05 -0500

    Set GA4 consent defaults before hydration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          id="eccoozs-consent-default"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              window.gtag = window.gtag || function(){ dataLayer.push(arguments); };
+              window.gtag('consent', 'default', {
+                analytics_storage: 'denied',
+                ad_storage: 'denied',
+                ad_user_data: 'denied',
+                ad_personalization: 'denied',
+                functionality_storage: 'granted',
+                security_storage: 'granted',
+                wait_for_update: 500
+              });
+            `,
+          }}
+        />
+      </head>
       <body>
         {children}
         <GoogleAnalyticsConsent />
